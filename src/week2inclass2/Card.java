b38cd31d0051365f581ff20e891539aa41cb8956
User1 <User1@10.250.12.193>
2017-01-18 12:52:12 -0500

    Initial Submission 2

---
+++ src/week2inclass2/Card.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week2inclass2;
+
+/**
+ *
+ * @author User1
+ */
+public class Card {
+    private String faceName, suit;
+    private int faceValue;
+    
+    public Card(String face, String suit, int value)
+    {
+        faceName = face; // faceName in pink means its a global variable
+        setSuit(suit);
+        setValue(value);
+    }//end of constructor
+    
+    // this method will return a sString to represent the Card
+    public String toString()
+    {
+        return faceName + " of " + suit;
+    
+    }// end of toString
+    
+    // This method will validate if the suit sis valie i.e hearts, diamonds, spades or clubs
+    private void setSuit(String suit)
+    {
+        if (suit.equalsIgnoreCase("hearts") || suit.equalsIgnoreCase("diamonds") || suit.equalsIgnoreCase("spades") || suit.equalsIgnoreCase("clubs"))
+        {
+            this.suit = suit;
+        
+        }
+        else 
+            throw new IllegalArgumentException ("Suit must be hearts, diamonds, spades, or clubs");
+                
+    }// end of setSuit
+    
+    
+    // this will validate the input is between 2 - 14 and set the card value
+    private void setValue(int value)
+    {
+        if (value < 2 || value > 14)
+            throw new IllegalArgumentException ("Face value must be 2-14");
+        else
+            faceValue = value;
+        
+    
+    }// end of setValue
+
+    public String getFaceName() {
+        return faceName;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getFaceValue() {
+        return faceValue;
+    }
+    
+    
+    
+}// end of Card
---
+++ src/week2inclass2/deckOfCards.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week2inclass2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *
+ * @author User1
+ */
+public class deckOfCards{
+    
+    private ArrayList<Card> deck;
+    
+    //this constructor is my deck of cards
+    public deckOfCards(){
+        
+        deck = new ArrayList<>();
+        String [] suits = {"hearts","spades","clubs","diamonds"};
+        String [] faceNames = {"two","three","four","five","six","seven","eight","nine","ten","jack","queen","king","ace"};
+        
+        
+        for (String suit : suits)// loops over suits w Enhanced for loop
+        {
+            for (int i=0; i < faceNames.length; i++)
+            {
+                Card newCard = new Card(faceNames[i],suit, i + 2);
+                deck.add(newCard);
+            }
+        
+        }
+    }// end of constructor
+
+
+    //this method will return astring representing the deck of cards
+    public String toString(){
+    
+        String deckString = "";
+        
+        for (Card card : deck)
+        {
+            deckString += card.toString() + "\n";
+        }
+     
+        return deckString;
+    }// end of toString
+    
+    
+    //this method will shuffle the deck of cards
+    public void shuffle(){
+        
+        Collections.shuffle(deck);
+    
+    }//end of shuffle
+    
+    // this method will deal top card off the deck
+    public Card dealTopCard(){
+      
+        if (deck.isEmpty())
+            return null;
+        else return deck.remove(0);
+    }
+    
+     
+            
+}// end of main
+
