c52862350396cf67a4fc54733d61f987b0b2802a
User1 <User1@10.250.12.193>
2017-01-18 13:34:44 -0500

    Initial Submission 1

---
+++ src/week2inclass2/gameOfWar.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week2inclass2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author User1
+ */
+public class gameOfWar { // this class consist of the rules for 'war'
+    private ArrayList<Card> p1Hand;
+    private ArrayList<Card> p2Hand;
+    
+    public gameOfWar(){
+   
+        p1Hand = new ArrayList<>();
+        p2Hand = new ArrayList<>();
+        
+        deckOfCards theDeck = new deckOfCards();
+        theDeck
+    }// end of GameOfWar
+    
+
+            
+}// end of class 
